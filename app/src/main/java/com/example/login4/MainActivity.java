129cba1a77b4d92a50cb55a22fb79a37a8257d87
Admin <k3.a4.tuananhh@gmail.com>
2024-02-24 09:25:52 +0700

    đã fix

--- app/src/main/java/com/example/login4/MainActivity.java
+++ app/src/main/java/com/example/login4/MainActivity.java
@@ -11,7 +11,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 // sửa lại code đi
-//tuấn anh dz vlin
+//tuấn anh dz vlin fpt
 
         ///đã cmt
     }

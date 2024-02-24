28eeb27dc7c7b7757dbceeefdd664c7e6cdeb7e4
Admin <k3.a4.tuananhh@gmail.com>
2024-02-24 09:32:03 +0700

    đã fix

--- app/src/main/java/com/example/login4/MainActivity.java
+++ app/src/main/java/com/example/login4/MainActivity.java
@@ -12,6 +12,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 // sửa lại code đi
 //tuấn anh dz vlin fpt
+        //fix code đi
 
         ///đã cmt
     }

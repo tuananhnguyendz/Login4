6e7e129cc9c53e580d8d0e2a9f7d8cdf877e0e7c
Admin <k3.a4.tuananhh@gmail.com>
2024-02-24 09:22:15 +0700

    sửa lại code đi

--- app/src/main/java/com/example/login4/MainActivity.java
+++ app/src/main/java/com/example/login4/MainActivity.java
@@ -11,6 +11,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 // sửa lại code đi
+//tuấn anh
 
         ///đã cmt
     }
